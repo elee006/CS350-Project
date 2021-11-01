b7b16d10827a71946b6f96c4a70f2813e08437b6
Korey <kharr098@odu.edu>
2021-10-31 22:49:59 -0400

    Fifth commit (main)

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,6 @@
 package main.java;
 import java.util.Scanner;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
@@ -17,6 +18,33 @@ System.out.print("Enter input file path and name:");
 String inFile = inScanner.next();
 System.out.println("You entered: " + inFile);           
 FileInputStream fs = new FileInputStream(inFile);
+        }
+    }
 }
+
+public  class DirectoryPath {
+        // Directory name entered by the user.
+        String directoryName;  
+        // File object referring to the directory.
+        File directory;        
+        // Array of file names in the directory.
+        String[] files;        
+        // For reading a line of input from the user.
+        Scanner scanner;       
+        scanner = new Scanner(System.in);  
+        System.out.print("Enter a directory name: ");
+        directoryName = scanner.nextLine().trim();
+        directory = new File(directoryName);
+        if (directory.isDirectory() == false) {
+            if (directory.exists() == false)
+                System.out.println("This directory does not exist...");
+            else
+                System.out.println("That file is not a directory.");
+        }
+        else {
+            files = directory.list();
+            System.out.println("Files in directory \"" + directory + "\":");
+            for (int i = 0; i < files.length; i++)
+                System.out.println("   " + files[i]);
 }
 }
