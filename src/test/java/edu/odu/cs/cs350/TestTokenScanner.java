aebe5b62ce33837f6fcf828d68b6a24d7abcf101
Erica Lee <elee006@odu.edu>
2021-12-05 20:13:42 -0500

    test tokenscanner

--- src/test/java/edu/odu/cs/cs350/TestTokenScanner.java
+++ src/test/java/edu/odu/cs/cs350/TestTokenScanner.java
@@ -9,26 +9,38 @@ import java.io.*;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.hamcrest.MatcherAssert.assertThat; 
-import static org.hamcrest.Matchers.*;
+
 /* https://git.cs.odu.edu/zeil/jflexdemo/-/blob/master/src/test/java/edu/odu/cs/cs350/jflexdemo/TestScanner.java
 */
 public class TestTokenScanner{
 
     @Test 
     public void testScanner(){
-        File file = new File("C:\\CS350-Project\\test.txt");
-        /*
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String inputString = null;
+        /*File file = new File("C:\\CS350-Project\\test.txt");*/
 
-        inputString = br.readLine();
+        
+       /* BufferedReader br = new BufferedReader(new FileReader(file));*/
+        String inputString = "int a 3";
+
+  /*inputString = br.readLine();*/
+         
 
         Reader input = new StringReader(inputString);
         TokenStream ts = new TokenStream(input);
         ArrayList<token> tokens = new ArrayList<token>();
+
+        for(token tok: ts){
+            tokens.add(tok);
+
+        }
+        assertEquals (3, tokens.size());
+
+        token t = tokens.get(0);
+        assertEquals (TokenTypes.IDENTIFIER, t.getType());
+
+
         
-*/
+
 
 
 
